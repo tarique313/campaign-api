5c1f6f437a143782d568c83b2f0e5d6bff2472cd
tarique313 <mahmud.tarique@gmail.com>
2022-05-23 17:33:26 +0600

    Remove show action from route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :campaigns, except: [:destroy] do
-        resources :investments, only: [:index, :create, :show, :update]
+        resources :investments, only: [:index, :create, :show]
       end
     end
   end
